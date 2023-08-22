535e23994fa3ef750a8dade1612656013b58be45
shravan77r <136837547+shravan77r@users.noreply.github.com>
2023-08-22 15:47:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
-
+const list = document.getElementById('infi-list');
+function addListItem(text) {
+  const li = document.createElement('li');
+  li.textContent = text;
+  list.appendChild(li);
+}
+for (let i = 1; i <= 10; i++) {
+  addListItem(`Item ${i}`);
+}
+function addMoreItems() {
+  for (let i = 1; i <= 2; i++) {
+    addListItem(`Item ${list.children.length + i}`);
+  }
+}
+list.addEventListener('scroll', function () {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addMoreItems();
+  }
+});
